e9edf7893020b156c1e6eed2e37fae17c9a29c25
Sanskar Bhardwaj <sanskarsrdav@gmail.com>
2025-01-27 01:30:53 +0530

    modified carousel

--- components/homepage/InfiniteSliderVertical.js
+++ components/homepage/InfiniteSliderVertical.js
@@ -5,116 +5,196 @@ export function InfiniteSliderVertical() {
     <div className="flex h-full space-x-2 sm:space-x-4 lg:space-x-8 mx-auto">
       <InfiniteSlider direction="vertical">
         <img
-          src="/carousel/1.jpg"
+          src="/carousel/1.png"
           alt="Dean blunt - Black Metal 2"
-          className="w-[200px] sm:w-[250px] md:w-[300px] lg:w-[350px] rounded-[4px] object-cover"
+          className="w-[200px] sm:w-[250px] md:w-[300px] lg:w-[380px] rounded-[4px] object-contain"
         />
         <img
-          src="/carousel/2.jpg"
+          src="/carousel/2.png"
           alt="Jungle Jack - JUNGLE DES ILLUSIONS VOL 2"
-          className="w-[200px] sm:w-[250px] md:w-[300px] lg:w-[350px] rounded-[4px] object-cover"
+          className="w-[200px] sm:w-[250px] md:w-[300px] lg:w-[380px] rounded-[4px] object-contain"
         />
         <img
           src="/carousel/3.png"
           alt="Yung Lean - Stardust"
-          className="w-[200px] sm:w-[250px] md:w-[300px] lg:w-[350px] rounded-[4px] object-cover"
+          className="w-[200px] sm:w-[250px] md:w-[300px] lg:w-[380px] rounded-[4px] object-contain"
         />
         <img
-          src="/carousel/4.PNG"
+          src="/carousel/4.png"
           alt="Lana Del Rey - Ultraviolence"
-          className="w-[200px] sm:w-[250px] md:w-[300px] lg:w-[350px] rounded-[4px] object-cover"
+          className="w-[200px] sm:w-[250px] md:w-[300px] lg:w-[380px] rounded-[4px] object-contain"
         />
         <img
-          src="/carousel/5.jpg"
+          src="/carousel/5.png"
           alt="A$AP Rocky - Tailor Swif"
-          className="w-[200px] sm:w-[250px] md:w-[300px] lg:w-[350px] rounded-[4px] object-cover"
+          className="w-[200px] sm:w-[250px] md:w-[300px] lg:w-[380px] rounded-[4px] object-contain"
         />
         <img
-          src="/carousel/6.jpg"
+          src="/carousel/6.png"
           alt="Midnight Miami (feat Konvy) - Nino Paid, Konvy"
-          className="w-[200px] sm:w-[250px] md:w-[300px] lg:w-[350px] rounded-[4px] object-cover"
+          className="w-[200px] sm:w-[250px] md:w-[300px] lg:w-[380px] rounded-[4px] object-contain"
         />
         <img
-          src="/carousel/8.jpg"
+          src="/carousel/9.png"
           alt="You're in My System - TORYONTHEBEAT"
-          className="w-[200px] sm:w-[250px] md:w-[300px] lg:w-[350px] rounded-[4px] object-cover"
+          className="w-[200px] sm:w-[250px] md:w-[300px] lg:w-[380px] rounded-[4px] object-contain"
         />
         <img
-          src="/carousel/9.jpg"
+          src="/carousel/9.png"
           alt="You can't tell me - People Make the World Go Round"
-          className="w-[200px] sm:w-[250px] md:w-[300px] lg:w-[350px] rounded-[4px] object-cover"
+          className="w-[200px] sm:w-[250px] md:w-[300px] lg:w-[380px] rounded-[4px] object-contain"
         />
         <img
-          src="/carousel/10.jpg"
+          src="/carousel/10.png"
           alt="ye - Kanye West"
-          className="w-[200px] sm:w-[250px] md:w-[300px] lg:w-[350px] rounded-[4px] object-cover"
+          className="w-[200px] sm:w-[250px] md:w-[300px] lg:w-[380px] rounded-[4px] object-contain"
         />
         <img
-          src="/carousel/11.jpg"
+          src="/carousel/11.png"
           alt="Slime Season 3 - Young Thug"
-          className="w-[200px] sm:w-[250px] md:w-[300px] lg:w-[350px] rounded-[4px] object-cover"
+          className="w-[200px] sm:w-[250px] md:w-[300px] lg:w-[380px] rounded-[4px] object-contain"
         />
         <img
-          src="/carousel/12.jpeg"
+          src="/carousel/12.png"
           alt="SWAG - 8ruki"
-          className="w-[200px] sm:w-[250px] md:w-[300px] lg:w-[350px] rounded-[4px] object-cover"
+          className="w-[200px] sm:w-[250px] md:w-[300px] lg:w-[380px] rounded-[4px] object-contain"
+        />
+        <img
+          src="/carousel/13.png"
+          alt="SWAG - 8ruki"
+          className="w-[200px] sm:w-[250px] md:w-[300px] lg:w-[380px] rounded-[4px] object-contain"
+        />
+        <img
+          src="/carousel/14.png"
+          alt="SWAG - 8ruki"
+          className="w-[200px] sm:w-[250px] md:w-[300px] lg:w-[380px] rounded-[4px] object-contain"
+        />
+        <img
+          src="/carousel/15.png"
+          alt="SWAG - 8ruki"
+          className="w-[200px] sm:w-[250px] md:w-[300px] lg:w-[380px] rounded-[4px] object-contain"
+        />
+        <img
+          src="/carousel/16.png"
+          alt="SWAG - 8ruki"
+          className="w-[200px] sm:w-[250px] md:w-[300px] lg:w-[380px] rounded-[4px] object-contain"
+        />
+        <img
+          src="/carousel/17.png"
+          alt="SWAG - 8ruki"
+          className="w-[200px] sm:w-[250px] md:w-[300px] lg:w-[380px] rounded-[4px] object-contain"
+        />
+        <img
+          src="/carousel/18.png"
+          alt="SWAG - 8ruki"
+          className="w-[200px] sm:w-[250px] md:w-[300px] lg:w-[380px] rounded-[4px] object-contain"
+        />
+        <img
+          src="/carousel/19.jpg"
+          alt="SWAG - 8ruki"
+          className="w-[200px] sm:w-[250px] md:w-[300px] lg:w-[380px] rounded-[4px] object-contain"
+        />
+        <img
+          src="/carousel/20.jpg"
+          alt="SWAG - 8ruki"
+          className="w-[200px] sm:w-[250px] md:w-[300px] lg:w-[380px] rounded-[4px] object-contain"
         />
       </InfiniteSlider>
       <InfiniteSlider direction="vertical" reverse>
         <img
-          src="/carousel/1.jpg"
+          src="/carousel/1.png"
           alt="Dean blunt - Black Metal 2"
-          className="w-[200px] sm:w-[250px] md:w-[300px] lg:w-[350px] rounded-[4px] object-cover"
+          className="w-[200px] sm:w-[250px] md:w-[300px] lg:w-[380px] rounded-[4px] object-cover"
         />
         <img
-          src="/carousel/2.jpg"
+          src="/carousel/2.png"
           alt="Jungle Jack - JUNGLE DES ILLUSIONS VOL 2"
-          className="w-[200px] sm:w-[250px] md:w-[300px] lg:w-[350px] rounded-[4px] object-cover"
+          className="w-[200px] sm:w-[250px] md:w-[300px] lg:w-[380px] rounded-[4px] object-cover"
         />
         <img
           src="/carousel/3.png"
           alt="Yung Lean - Stardust"
-          className="w-[200px] sm:w-[250px] md:w-[300px] lg:w-[350px] rounded-[4px] object-cover"
+          className="w-[200px] sm:w-[250px] md:w-[300px] lg:w-[380px] rounded-[4px] object-cover"
         />
         <img
-          src="/carousel/4.PNG"
+          src="/carousel/4.png"
           alt="Lana Del Rey - Ultraviolence"
-          className="w-[200px] sm:w-[250px] md:w-[300px] lg:w-[350px] rounded-[4px] object-cover"
+          className="w-[200px] sm:w-[250px] md:w-[300px] lg:w-[380px] rounded-[4px] object-cover"
         />
         <img
-          src="/carousel/5.jpg"
+          src="/carousel/5.png"
           alt="A$AP Rocky - Tailor Swif"
-          className="w-[200px] sm:w-[250px] md:w-[300px] lg:w-[350px] rounded-[4px] object-cover"
+          className="w-[200px] sm:w-[250px] md:w-[300px] lg:w-[380px] rounded-[4px] object-cover"
         />
         <img
-          src="/carousel/6.jpg"
+          src="/carousel/6.png"
           alt="Midnight Miami (feat Konvy) - Nino Paid, Konvy"
-          className="w-[200px] sm:w-[250px] md:w-[300px] lg:w-[350px] rounded-[4px] object-cover"
+          className="w-[200px] sm:w-[250px] md:w-[300px] lg:w-[380px] rounded-[4px] object-cover"
         />
         <img
-          src="/carousel/8.jpg"
+          src="/carousel/8.png"
           alt="You're in My System - TORYONTHEBEAT"
-          className="w-[200px] sm:w-[250px] md:w-[300px] lg:w-[350px] rounded-[4px] object-cover"
+          className="w-[200px] sm:w-[250px] md:w-[300px] lg:w-[380px] rounded-[4px] object-cover"
         />
         <img
-          src="/carousel/9.jpg"
+          src="/carousel/9.png"
           alt="You can't tell me - People Make the World Go Round"
-          className="w-[200px] sm:w-[250px] md:w-[300px] lg:w-[350px] rounded-[4px] object-cover"
+          className="w-[200px] sm:w-[250px] md:w-[300px] lg:w-[380px] rounded-[4px] object-cover"
         />
         <img
-          src="/carousel/10.jpg"
+          src="/carousel/10.png"
           alt="ye - Kanye West"
-          className="w-[200px] sm:w-[250px] md:w-[300px] lg:w-[350px] rounded-[4px] object-cover"
+          className="w-[200px] sm:w-[250px] md:w-[300px] lg:w-[380px] rounded-[4px] object-cover"
         />
         <img
-          src="/carousel/11.jpg"
+          src="/carousel/11.png"
           alt="Slime Season 3 - Young Thug"
-          className="w-[200px] sm:w-[250px] md:w-[300px] lg:w-[350px] rounded-[4px] object-cover"
+          className="w-[200px] sm:w-[250px] md:w-[300px] lg:w-[380px] rounded-[4px] object-cover"
         />
         <img
-          src="/carousel/12.jpeg"
+          src="/carousel/12.png"
           alt="SWAG - 8ruki"
-          className="w-[200px] sm:w-[250px] md:w-[300px] lg:w-[350px] rounded-[4px] object-cover"
+          className="w-[200px] sm:w-[250px] md:w-[300px] lg:w-[380px] rounded-[4px] object-cover"
+        />
+        <img
+          src="/carousel/13.png"
+          alt="SWAG - 8ruki"
+          className="w-[200px] sm:w-[250px] md:w-[300px] lg:w-[380px] rounded-[4px] object-cover"
+        />
+        <img
+          src="/carousel/14.png"
+          alt="SWAG - 8ruki"
+          className="w-[200px] sm:w-[250px] md:w-[300px] lg:w-[380px] rounded-[4px] object-cover"
+        />
+        <img
+          src="/carousel/15.png"
+          alt="SWAG - 8ruki"
+          className="w-[200px] sm:w-[250px] md:w-[300px] lg:w-[380px] rounded-[4px] object-cover"
+        />
+        <img
+          src="/carousel/16.png"
+          alt="SWAG - 8ruki"
+          className="w-[200px] sm:w-[250px] md:w-[300px] lg:w-[380px] rounded-[4px] object-cover"
+        />
+        <img
+          src="/carousel/17.png"
+          alt="SWAG - 8ruki"
+          className="w-[200px] sm:w-[250px] md:w-[300px] lg:w-[380px] rounded-[4px] object-cover"
+        />
+        <img
+          src="/carousel/18.png"
+          alt="SWAG - 8ruki"
+          className="w-[200px] sm:w-[250px] md:w-[300px] lg:w-[380px] rounded-[4px] object-cover"
+        />
+        <img
+          src="/carousel/19.jpg"
+          alt="SWAG - 8ruki"
+          className="w-[200px] sm:w-[250px] md:w-[300px] lg:w-[380px] rounded-[4px] object-contain"
+        />
+        <img
+          src="/carousel/20.jpg"
+          alt="SWAG - 8ruki"
+          className="w-[200px] sm:w-[250px] md:w-[300px] lg:w-[380px] rounded-[4px] object-contain"
         />
       </InfiniteSlider>
     </div>
