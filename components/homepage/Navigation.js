45b7ae42ffcfb09ca2c4316c9bbf9a085b7e7856
Sanskar Bhardwaj <sanskarsrdav@gmail.com>
2025-03-15 01:20:22 +0530

    ...

--- components/homepage/Navigation.js
+++ components/homepage/Navigation.js
@@ -1,4 +1,4 @@
-import { Home, User, Briefcase, FileText, Eclipse, Handshake, Phone } from "lucide-react";
+import { Home, User, Briefcase, FileText, Eclipse, Handshake, Phone , Video} from "lucide-react";
 import { NavBar } from "@/components/ui/tubelight-navbar";
 import { usePathname } from "next/navigation";
 
@@ -8,8 +8,9 @@ export default function Navigation() {
   const navItems = [
     { name: "Home", url: "/", icon: Home, active: pathname === "/" },
     { name: "Nights", url: "/thenights", icon: Eclipse, active: pathname === "/thenights" },
-    { name: "Contact", url: "/contactPage/contact", icon: Phone, active: pathname === "/contact" },
     { name: "Dashboard", url: "/Dashboard", icon: User, active: pathname === "/Dashboard" },
+    { name: "Contact", url: "/contactPage/contact", icon: Phone, active: pathname === "/contact" },
+    { name: "S1 Demos", url: "/demoday", icon: Video, active: pathname === "/demoday" },
   ];
 
   return <NavBar items={navItems} />;
