4bf9a4e41516e33c00dbf345898a7f53bd7d059a
Rishabh <rishabhrawat.171105@gmail.com>
2026-01-15 16:51:28 +0530

    Added Join Now Page

---
+++ pages/join-now.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import JoinPage from '@/components/joinPage/JoinPage'
+
+export default function JoinNow() {
+  return (
+    <div>
+      <JoinPage />
+    </div>
+  )
+}
